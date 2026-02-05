0fdb4f7089adb6193a7d8bdef8e4de19e17de674
Tushar Sahu <tusharsahu1453@gmail.com>
2026-02-05 22:58:50 +0530

    Update firebase.js to remove sensitive config
    Removed sensitive Firebase configuration details and replaced with a placeholder.

--- firebase.js
+++ firebase.js
@@ -1,13 +1,7 @@
 // ================= FIREBASE CONFIG =================
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAMaYeu_N6PO77ub29Pi3wZKNd0Fof1D44",
-  authDomain: "goal-planner-8330b.firebaseapp.com",
-  projectId: "goal-planner-8330b",
-  storageBucket: "goal-planner-8330b.firebasestorage.app",
-  messagingSenderId: "869435593362",
-  appId: "1:869435593362:web:2e5be829f5c6f177c34c4b",
-  measurementId: "G-84T15NJFL1"
+    YOUR API KEY
 };
 
 // ================= INITIALIZE FIREBASE =================
@@ -17,3 +11,4 @@ firebase.initializeApp(firebaseConfig);
 // 🔥 EXPOSE THESE GLOBALLY
 window.db = firebase.firestore();
 window.auth = firebase.auth();
+
